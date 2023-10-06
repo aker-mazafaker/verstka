ed0651bd56f67641176a4607cdaa94a64d426ed3
Aker <@akerke.n@inbox.ru>
2023-10-06 13:19:50 +0600

    Initial Commit

---
+++ verstka/verstkaApp.swift
@@ -0,0 +1,17 @@
+//
+//  verstkaApp.swift
+//  verstka
+//
+//  Created by Akerke on 06.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct verstkaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
